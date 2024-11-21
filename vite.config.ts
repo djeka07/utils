e7049370eb03c157cc24473bac864846f00d6fa1
Andre Ekbom <andreekbom@live.se>
2024-11-21 11:14:22 +0100

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
 import { resolve } from 'path';
 import preserveDirectives from 'rollup-preserve-directives';
 import { defineConfig, Plugin } from 'vite';
+import { copyFileSync } from 'node:fs';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     preserveDirectives() as Plugin,
-    dts({ include: 'src', exclude: ['**/*.test.ts'], insertTypesEntry: true }),
+    dts({
+      include: 'src',
+      exclude: ['**/*.stories.tsx'],
+      insertTypesEntry: true,
+      rollupTypes: true,
+      afterBuild: () => {
+        copyFileSync('dist/index.d.cts', 'dist/index.d.mts');
+      },
+    }),
     tsconfigPaths(),
   ],
   build: {
     copyPublicDir: false,
+    minify: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'js' : format}`,
+      fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'mjs' : format}`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
